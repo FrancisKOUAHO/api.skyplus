599f6e375a81d810c4f7104f413c1aad2eb6c5d0
Arboos-naori <kouaho@arboos.com>
2021-10-06 03:17:12 +0200

    managed to get the linkedin data and put it in an Excel file

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import {get_infos_users} from "./methods/get_infos_users";
         await sleep_for(page, 1000, 2000)
 
         await authenticate(page)
-        await sleep_for(page, 10000, 20000)
+        await sleep_for(page, 9000, 1500)
         await search(page)
         await click_button(page, 'li.search-reusables__primary-filter')
 
@@ -31,14 +31,16 @@ import {get_infos_users} from "./methods/get_infos_users";
         console.log(DATA_USERS)
         await sleep_for(page, 500, 1000)
 
-        const DATA_GET_USERS = DATA_USERS.map(user => [user])
-
 
         const wb = xlsx.utils.book_new()
-        const ws = xlsx.utils.aoa_to_sheet(DATA_GET_USERS)
+        const ws = xlsx.utils.json_to_sheet(DATA_USERS)
         xlsx.utils.book_append_sheet(wb, ws)
         xlsx.writeFile(wb, 'data_linkedin.xlsx')
 
+        await sleep_for(page, 500, 1000)
+        await browser.close()
+
+        console.log("Done!")
     } catch (e) {
         console.log(e)
     }
